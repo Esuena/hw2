e0f88c4b96e73c4d633fc70af92e63223063e69d
Esuena <esuena@linux-yr1j.site>
2015-11-04 23:33:43 +0100

    Finally done with HW 2 !!!

--- logmap.cxx
+++ logmap.cxx
@@ -1,23 +1,36 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(){
-	double x0 = 0.5;
-	double x;
-	int Nskip = 100; //Number of iterations to skip
-	int Nend  = 200; //Number of total iterations
-
-	for(double r=0; r <= 4; r += 0.001){
-	   x=x0;
-	   for(int i=1; i <= Nskip; i++)
-		   x = r*x*(1-x);
-	   for(int i=Nskip+1; i <= Nend; i++){
-	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
-   	   }
-	}
-
-
-	return 0;
+  double x;
+  double r;
+  int Nskip=100; //number of iterations to skip
+  int Nend=200; //number of total  iterations
+  int M=4;
+  
+  int ArraySize=Nend-Nskip;
+  double p[ArraySize]; 
+  
+  ofstream out("output.dat");
+  
+  for(r=1.0;r<=M;r+=0.01){
+    x=0.5;
+      for(int i=1;i<=Nskip;i++){
+	x=r*x*(1-x);	
+      }
+	for(int k=Nskip+1;k<=Nend;k++){ 
+	  x=r*x*(1-x);
+	  p[k-(Nskip+1)]=x; //store values in the array
+	  //cout << r <<"\t" << x << endl;
+	}  
+	 for(int i=0;i<ArraySize;i++){
+	   //cout << r << '\t' << "p[" << i << "]=" << p[i] << endl;
+	   out << r << '\t' << p[i] << endl; //write all values in file
+	 }
+  }
+  out.close();
+  
+  return 0; 
 }
